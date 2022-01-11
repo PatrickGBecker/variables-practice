6f14778889dd609900c0ea3675ab39cbf5ac7099
Patrick Becker <patrickgbecker@gmail.com>
2022-01-10 21:25:40 -0600

    Add annotations

--- variables.js
+++ variables.js
@@ -1,6 +1,14 @@
+// variable wizard is declared and assigned to the string of "Gandalf"
 var wizard = "Gandalf"
+// variable ranger is declared and assigned to the string of "Aragorn"
 var ranger = "Aragorn, son of Arathorn, Heir to the throne of Gondor"
+// variable gandaldAge is declared and assigned to the number of 3000
 var gandalfAge = 3000
+// variable aragornAge is declared and assigned to the number of 167
 var aragornAge = 167
+// variable canCarryRing is declared and assigned to the boolean of false
 var canCarryRing = false
+// variable canCarryFrodo is declared and assigned to the boolean of true
 var canCarryFrodo = true
+// Declared variables with strings, numbers and booleans.
+// Will write code later.
