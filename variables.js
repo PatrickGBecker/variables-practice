2d8e4677eba431e34e57fbcb77c1762348fe498f
Patrick Becker <patrickgbecker@gmail.com>
2022-01-10 21:19:06 -0600

    Add booleans

--- variables.js
+++ variables.js
@@ -2,3 +2,5 @@ var wizard = "Gandalf"
 var ranger = "Aragorn, son of Arathorn, Heir to the throne of Gondor"
 var gandalfAge = 3000
 var aragornAge = 167
+var canCarryRing = false
+var canCarryFrodo = true
